f1f048ee85f6174bc3f4f40d2821e0f107c80903
Alex Gap <alex.gap@gmail.com>
2021-08-31 14:07:07 -0700

    simple sample

--- contracts/sample.sol
+++ contracts/sample.sol
@@ -1,11 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
-
 pragma solidity ^0.8.7;
 
-contract SimpleSample {
+contract Sample {
     uint256 public total;
 
-    function addOne() public payable {
-        total = total + 1;
+    function add(uint256 amountToAdd) public payable {
+        total = total + amountToAdd;
     }
 }
